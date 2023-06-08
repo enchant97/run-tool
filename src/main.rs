b32cd5b26ef058217d753513cd31b8d1e0ce3eee
Leo Spratt <enchant97@users.noreply.github.com>
2023-06-08 21:12:55 +0100

    use proper exit codes for errors

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,14 @@ fn main() {
             Some(v) => v,
             None => {
                 eprintln!("could not read run configuration");
-                exit(1);
+                exit(exitcode::IOERR);
             }
         };
     let run_config: Config = match serde_yaml::from_str(&run_config_raw) {
         Ok(v) => v,
         Err(_) => {
             eprintln!("failed to parse configuration");
-            exit(1);
+            exit(exitcode::CONFIG);
         }
     };
     match args.command {
@@ -35,20 +35,20 @@ fn main() {
         } => {
             let app_exe_path = current_exe().unwrap_or_else(|_| {
                 eprintln!("failed to get current executable path");
-                exit(1);
+                exit(exitcode::OSERR);
             });
             let run_config = match run_config.configurations.get(&config_name) {
                 Some(v) => v,
                 None => {
                     eprintln!("run configuration not found");
-                    exit(1);
+                    exit(exitcode::USAGE);
                 }
             };
             let file_envs = match helpers::read_env_files(&run_config.env_files()) {
                 Ok(v) => v,
                 Err(_) => {
                     eprintln!("failed to parse environment files");
-                    exit(1);
+                    exit(exitcode::DATAERR);
                 }
             };
 
@@ -58,12 +58,12 @@ fn main() {
                     .spawn()
                     .unwrap_or_else(|err| {
                         eprintln!("failed to run '{}': {}", name, err);
-                        exit(1);
+                        exit(exitcode::SOFTWARE);
                     })
                     .wait()
                     .unwrap_or_else(|_| {
                         eprintln!("failed to execute");
-                        exit(1);
+                        exit(exitcode::OSERR);
                     });
                 if !process.success() {
                     exit(process.code().unwrap_or_default())
@@ -88,12 +88,12 @@ fn main() {
                 .spawn()
                 .unwrap_or_else(|err| {
                     eprintln!("{}", err);
-                    exit(1);
+                    exit(exitcode::SOFTWARE);
                 })
                 .wait()
                 .unwrap_or_else(|_| {
                     eprintln!("failed to execute");
-                    exit(1);
+                    exit(exitcode::OSERR);
                 });
             if !process.success() {
                 exit(process.code().unwrap_or_default())
